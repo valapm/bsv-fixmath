922c4200de57a9ec268ffe0fd680c871ef51b56e
Merlin <zezbvk@frenmail.com>
2022-10-16 21:30:39 +0200

    Remove unused script

--- compile.js
+++
@@ -1,9 +0,0 @@
-const path = require("path")
-const { compileContract } = require("scryptlib")
-
-const filePath = path.join(__dirname, "scrypt/fixMath.scrypt")
-const out = path.join(__dirname, "dist")
-
-const result = compileContract(filePath, {
-  out
-})
